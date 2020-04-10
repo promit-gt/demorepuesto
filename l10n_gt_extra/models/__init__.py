85ad38f99951008f4e6acb9c2e3f6f33e6f6b9da
Kevturac <47229465+kevturpromit@users.noreply.github.com>
2020-04-10 13:07:08 -0600

    l10
    Modulo contable

---
+++ l10n_gt_extra/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+
+from . import account
+from . import res_partner
+from . import report
+from . import l10n_gt_extra
+from . import purchase
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
